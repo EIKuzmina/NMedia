1e6b03debe63afdcb7e59c87cb3c08cbcaabdd03
Kuzmina Ekaterina Igorevna <Sterteliti@yandex.ru>
2024-08-20 15:34:00 +0300

    Fragments1

--- app/src/main/java/ru/netology/nmedia/activity/EditPostFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/EditPostFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -29,11 +30,16 @@ class EditPostFragment : Fragment() {
         arguments?.textArg?.let(binding.edit::setText)
         binding.ok.setOnClickListener {
             val text = binding.edit.text.toString()
-                viewModel.changeContent(text)
-                viewModel.save()
-                AndroidUtils.hideKeyboard(requireView())
-                findNavController().navigateUp()
-            }
+            viewModel.changeContent(text)
+            viewModel.save()
+            AndroidUtils.hideKeyboard(requireView())
+            findNavController().navigateUp()
+        }
+
+        activity?.onBackPressedDispatcher?.addCallback(viewLifecycleOwner) {
+            viewModel.clearEdit()
+            findNavController().navigateUp()
+        }
         return binding.root
     }
 }
