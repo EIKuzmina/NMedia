9c815bc3dfe8f579d8f0defa695c7e4091334b3a
Kuzmina Ekaterina Igorevna <Sterteliti@yandex.ru>
2024-10-14 14:40:29 +0300

    Notifications

---
+++ app/src/main/java/ru/netology/nmedia/service/FCMService.kt
@@ -0,0 +1,149 @@
+package ru.netology.nmedia.service
+
+import android.Manifest
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.google.gson.Gson
+import ru.netology.nmedia.R
+import ru.netology.nmedia.activity.AppActivity
+import kotlin.random.Random
+
+
+class FCMService : FirebaseMessagingService() {
+    private val channelId = "channel"
+    private val gson = Gson()
+
+    override fun onCreate() {
+        super.onCreate()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_remote_name)
+            val descriptionText = getString(R.string.channel_remote_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, name, importance).apply {
+                description = descriptionText
+            }
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        message.data["action"]?.let {
+            try {
+                when (Actions.valueOf(it)) {
+                    Actions.LIKE -> handleLike(
+                        gson.fromJson(
+                            message.data["content"],
+                            Like::class.java
+                        )
+                    )
+
+                    Actions.NEW_POST -> newPost(
+                        gson.fromJson(
+                            message.data["content"],
+                            NewPost::class.java
+                        )
+                    )
+                }
+            } catch (e: RuntimeException) {
+                println("Error!")
+            }
+        }
+    }
+
+    private fun handleLike(like: Like) {
+        val resultIntent = Intent(this, AppActivity::class.java)
+        val resultPendingIntent: PendingIntent? = TaskStackBuilder.create(this).run {
+            addNextIntentWithParentStack(resultIntent)
+            getPendingIntent(
+                0,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+        }
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle(
+                getString(
+                    R.string.notification_user_liked,
+                    like.userName,
+                    like.postAuthor,
+                )
+            )
+            .setContentIntent(resultPendingIntent)
+            .setAutoCancel(true)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+        checkPermission()
+        NotificationManagerCompat.from(this).notify(Random.nextInt(1_000), notification)
+    }
+
+    private fun newPost(post: NewPost) {
+        val resultIntent = Intent(this, AppActivity::class.java)
+        val resultPendingIntent: PendingIntent? = TaskStackBuilder.create(this).run {
+            addNextIntentWithParentStack(resultIntent)
+            getPendingIntent(
+                0,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+        }
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle(
+                getString(
+                    R.string.notification_new_post,
+                    post.userName,
+                    post.content
+                )
+            )
+            .setContentIntent(resultPendingIntent)
+            .setAutoCancel(true)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setStyle(NotificationCompat.BigTextStyle().bigText(post.content))
+            .build()
+        checkPermission()
+        NotificationManagerCompat.from(this).notify(Random.nextInt(100_000), notification)
+    }
+
+    override fun onNewToken(token: String) {
+        println(token)
+    }
+
+    private fun checkPermission() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+    }
+}
+
+enum class Actions {
+    LIKE, NEW_POST
+}
+
+data class Like(
+    val userId: Int,
+    val userName: String,
+    val postId: Int,
+    val postAuthor: String,
+)
+
+data class NewPost(
+    val userId: Int,
+    val userName: String,
+    val postId: Int,
+    val content: String
+)
