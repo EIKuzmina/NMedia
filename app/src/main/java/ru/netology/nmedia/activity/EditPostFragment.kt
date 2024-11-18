e5ed9ccc7a0750bdeadd0bb4227fefdb42796d07
Kuzmina Ekaterina Igorevna <Sterteliti@yandex.ru>
2024-11-18 11:38:03 +0300

    Retrofit CRUD four

--- app/src/main/java/ru/netology/nmedia/activity/EditPostFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/EditPostFragment.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import ru.netology.nmedia.model.PostViewModel
 import ru.netology.nmedia.databinding.FragmentEditPostBinding
-import ru.netology.nmedia.util.AndroidUtils
 import ru.netology.nmedia.util.StringArg
 
 class EditPostFragment : Fragment() {
@@ -24,14 +22,15 @@ class EditPostFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val binding = FragmentEditPostBinding.inflate(inflater, container, false)
-
         val viewModel: PostViewModel by viewModels(ownerProducer = ::requireParentFragment)
         binding.edit.requestFocus()
         arguments?.textArg?.let(binding.edit::setText)
+
         binding.ok.setOnClickListener {
             val text = binding.edit.text.toString()
             viewModel.changeContent(text)
             viewModel.save()
+            viewModel.draftContent("")
             findNavController().navigateUp()
         }
 
--- app/src/main/java/ru/netology/nmedia/activity/NewPostFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/NewPostFragment.kt
@@ -5,13 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import com.google.android.material.snackbar.Snackbar
-import ru.netology.nmedia.R
 import ru.netology.nmedia.model.PostViewModel
 import ru.netology.nmedia.databinding.FragmentNewPostBinding
-import ru.netology.nmedia.util.AndroidUtils
 import ru.netology.nmedia.util.StringArg
 
 class NewPostFragment : Fragment() {
@@ -19,14 +16,14 @@ class NewPostFragment : Fragment() {
         var Bundle.textArg by StringArg
     }
 
-    private val viewModel: PostViewModel by activityViewModels()
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val binding = FragmentNewPostBinding.inflate(inflater, container, false)
+        val viewModel: PostViewModel by viewModels(ownerProducer = ::requireParentFragment)
+        binding.editSave.requestFocus()
         arguments?.textArg?.let(binding.editSave::setText)
 
         binding.okSave.setOnClickListener {
@@ -34,8 +31,8 @@ class NewPostFragment : Fragment() {
             if (text.isNotBlank()) {
                 viewModel.changeContent(text)
                 viewModel.save()
-                    viewModel.draftContent("")
-                AndroidUtils.hideKeyboard(requireView())
+                viewModel.draftContent("")
+                findNavController().navigateUp()
             }
         }
 
